dfbd8d1e843cb945363c657a2c021095c5f4026d
that9jaboy3 <olatunjia889@gmail.com>
2023-07-10 14:01:20 +0100

    style:change noteStyle

--- mainNote.js
+++ mainNote.js
@@ -14,18 +14,33 @@ function addnote() {
 
 function addnt() {
     var list = document.createElement('div');
+    list.id = 'lit'
+
+    var can1 = document.createElement('button');
+    var cancel = document.createTextNode('Edit Note');
+    can1.appendChild(cancel);
+    // list.appendChild(can1);
+
 
     var but = document.createElement('button');
-    but.id = 'juj';
+    but.id = 'jujd';
     var del = document.createTextNode('Delete');
     but.appendChild(del);
-    list.appendChild(but);
+    // list.appendChild(but);
 
     var out = document.getElementById('area').value;
+    
     var add = document.createTextNode(out);
-
+  
     list.appendChild(add);
+ 
     document.getElementById('neww').appendChild(list);
+    document.getElementById('neww').appendChild(can1);
+    document.getElementById('neww').appendChild(but);
+
+
+
+
 
     var zero = '';
     document.getElementById('area').value = zero;
@@ -35,6 +50,10 @@ function addnt() {
     list.style.wordBreak = 'break-word';
     list.style.fontFamily = "'Hanken Grotesk', sans-serif";
     but.style.marginRight = '3%';
+    can1.style.marginRight = '3%';
+    can1.style.backgroundColor = 'gray';
+    can1.style.border = 'none';
+    can1.style.borderRadius = '3px';
     list.style.marginBottom = '10px';
     list.style.marginLeft = '5%';
     list.style.border = '1px solid black';
@@ -43,6 +62,7 @@ function addnt() {
     list.style.width = '60%';
     list.style.position = 'relative';
     list.style.padding = '30px';
+    list.style.marginTop ='2%';
 
 
     document.getElementById('fidd').innerHTML = 
@@ -56,6 +76,9 @@ function addnt() {
 
     but.onclick = function(){
         list.style.display = 'none';
+        but.style.display ='none';
+        can1.style.display = 'none';
+
         Number(document.getElementById('fidd').innerHTML) - 1;
     document.getElementById('fidd').innerHTML = 
     Number(document.getElementById('fidd').innerHTML) -1;
@@ -64,6 +87,38 @@ function addnt() {
     }
    
     }
+
+    can1.onclick = function(){
+        var edit = document.getElementById('lit').innerText;
+        document.getElementById('area').value = edit;
+        document.getElementById('notereview').style.display = 'none';
+        list.style.display = 'none';
+        but.style.display ='none';
+        can1.style.display = 'none';
+        document.getElementById('newNote').style.display = 'block';
+        document.getElementById('fidd').innerHTML = 
+        Number(document.getElementById('fidd').innerHTML) -1;
+        if (document.getElementById('fidd').innerHTML == '0') {
+          document.getElementById('fidd').style.display = 'none';
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
